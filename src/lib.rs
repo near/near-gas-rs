9d682662fea9f553982cbc1ec46fb42e383bb1dc
Ivan <serhieievivan6@gmail.com>
2023-05-02 20:17:01 +0300

    Errors 2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-
-
 pub const ONE_TERA_GAS: u64 = 10u64.pow(12);
 pub const ONE_GIGA_GAS: u64 = 10u64.pow(9);
 
@@ -51,56 +49,45 @@ impl NearGas {
     }
 }
 
-
 pub fn parse_str(s: &str, pref_const: u64) -> Result<u64, NearGasParsingError> {
     let (int, fract) = if let Some((whole, fractional)) = s.trim().split_once('.') {
-        let int: u64 = whole.parse().map_err(|_| NearGasParsingError::InvalidNumber(whole.to_owned()))?;
-        let mut fract: u64 = fractional.parse().map_err(|_| NearGasParsingError::InvalidNumber(fractional.to_owned()))?;
+        let int: u64 = whole
+            .parse()
+            .map_err(|_| NearGasParsingError::InvalidNumber(whole.to_owned()))?;
+        let mut fract: u64 = fractional
+            .parse()
+            .map_err(|_| NearGasParsingError::InvalidNumber(fractional.to_owned()))?;
         let len = fractional.len() as u32;
-        fract = if let Some(fract) = fract.checked_mul(
-            if let Some(mantiss) = pref_const.checked_div(10u64.pow(len)) {
-                mantiss
-            } else {
-                return Err(NearGasParsingError::LongFractional(fract.to_owned()));
-            },
-        ) {
-            fract
-        } else {
-            return Err(NearGasParsingError::LongFractional(fract.to_owned()));
-        };
+        fract = fract
+            .checked_mul(
+                pref_const
+                    .checked_div(10u64.pow(len))
+                    .ok_or(NearGasParsingError::LongFractional(fract.to_owned()))?,
+            )
+            .ok_or(NearGasParsingError::LongFractional(fract.to_owned()))?;
         (int, fract)
     } else {
-        let int: u64 = s.parse().map_err(|_|NearGasParsingError::InvalidNumber(s.to_owned()))?;
+        let int: u64 = s
+            .parse()
+            .map_err(|_| NearGasParsingError::InvalidNumber(s.to_owned()))?;
         (int, 0)
     };
-    let result = if let Some(result) =
-        fract.checked_add(if let Some(whole) = int.checked_mul(pref_const) {
-            whole
-        } else {
-            return Err(NearGasParsingError::LongWhole(int.to_owned()));
-        }) {
-        result
-    } else {
-        return Err(NearGasParsingError::LongWhole(int.to_owned()));
-    };
+    let result = fract
+        .checked_add(
+            int.checked_mul(pref_const)
+                .ok_or(NearGasParsingError::LongWhole(int.to_owned()))?,
+        )
+        .ok_or(NearGasParsingError::LongWhole(int.to_owned()))?;
     Ok(result)
 }
 
-
-
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum NearGasParsingError{
-    InvalidNumber(String),  
+pub enum NearGasParsingError {
+    InvalidNumber(String),
     LongWhole(u64),
     LongFractional(u64),
     IncorectCurrency(String),
 }
-
-  
-
-
-
-
 
 #[cfg(test)]
 mod test {
@@ -126,10 +113,13 @@ mod test {
         let test_data = "hnim";
         let gas = parse_str(test_data, ONE_GIGA_GAS);
         println!("{:?}", gas);
-        assert_eq!(gas, Err(NearGasParsingError::InvalidNumber("hnim".to_string())) )
+        assert_eq!(
+            gas,
+            Err(NearGasParsingError::InvalidNumber("hnim".to_string()))
+        )
     }
     #[test]
-    fn parse_u64_errortest(){
+    fn parse_u64_errortest() {
         let test_data = u64::MAX.to_string();
         let gas = parse_str(&test_data, ONE_GIGA_GAS);
         assert_eq!(gas, Err(NearGasParsingError::LongWhole(u64::MAX)));
