ab7d3fd530afb5163ff24079935f356782071d6a
Ivan <serhieievivan6@gmail.com>
2023-05-02 19:05:24 +0300

    Error refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-const ONE_TERA_GAS: u64 = 10u64.pow(12);
-const ONE_GIGA_GAS: u64 = 10u64.pow(9);
+
+
+pub const ONE_TERA_GAS: u64 = 10u64.pow(12);
+pub const ONE_GIGA_GAS: u64 = 10u64.pow(9);
 
 #[derive(Debug, Clone, Default, PartialEq, Eq)]
 pub struct NearGas {
@@ -7,14 +9,14 @@ pub struct NearGas {
 }
 
 impl std::str::FromStr for NearGas {
-    type Err = String;
+    type Err = NearGasParsingError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let num = s.trim().trim_end_matches(char::is_alphabetic).trim();
         let currency = s.trim().trim_start_matches(num).trim().to_uppercase();
         let number = match currency.as_str() {
             "T" | "TGAS" | "TERAGAS" => parse_str(num, ONE_TERA_GAS)?,
             "GIGAGAS" | "GGAS" => parse_str(num, ONE_GIGA_GAS)?,
-            _ => return Err("Near Gas: incorrect currency value entered".to_string()),
+            _ => return Err(NearGasParsingError::IncorectCurrency(currency.to_owned())),
         };
         let gas = NearGas::from_gas(number);
         Ok(gas)
@@ -49,48 +51,56 @@ impl NearGas {
     }
 }
 
-pub fn parse_str(s: &str, pref_const: u64) -> Result<u64, String> {
+
+pub fn parse_str(s: &str, pref_const: u64) -> Result<u64, NearGasParsingError> {
     let (int, fract) = if let Some((whole, fractional)) = s.trim().split_once('.') {
-        let int: u64 = match whole.parse() {
-            Ok(int) => int,
-            Err(_) => return Err("Parsing error: Invalid Data".to_string()),
-        };
-        let mut fract: u64 = match fractional.parse() {
-            Ok(fract) => fract,
-            Err(_) => return Err("Parsing error: Invalid Data".to_string()),
-        };
+        let int: u64 = whole.parse().map_err(|_| NearGasParsingError::InvalidNumber(whole.to_owned()))?;
+        let mut fract: u64 = fractional.parse().map_err(|_| NearGasParsingError::InvalidNumber(fractional.to_owned()))?;
         let len = fractional.len() as u32;
         fract = if let Some(fract) = fract.checked_mul(
             if let Some(mantiss) = pref_const.checked_div(10u64.pow(len)) {
                 mantiss
             } else {
-                return Err("Parsing Error: Too Long fractional part".to_string());
+                return Err(NearGasParsingError::LongFractional(fract.to_owned()));
             },
         ) {
             fract
         } else {
-            return Err("Parsing Error: Too Long fractional part".to_string());
+            return Err(NearGasParsingError::LongFractional(fract.to_owned()));
         };
         (int, fract)
     } else {
-        let int: u64 = match s.parse() {
-            Ok(int) => int,
-            Err(_) => return Err("Parsing error: Invalid Data".to_string()),
-        };
+        let int: u64 = s.parse().map_err(|_|NearGasParsingError::InvalidNumber(s.to_owned()))?;
         (int, 0)
     };
     let result = if let Some(result) =
         fract.checked_add(if let Some(whole) = int.checked_mul(pref_const) {
             whole
         } else {
-            return Err("Parsing Error: Too long whole part".to_string());
+            return Err(NearGasParsingError::LongWhole(int.to_owned()));
         }) {
         result
     } else {
-        return Err("Parsing Error: Too long whole part".to_string());
+        return Err(NearGasParsingError::LongWhole(int.to_owned()));
     };
     Ok(result)
 }
+
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum NearGasParsingError{
+    InvalidNumber(String),  
+    LongWhole(u64),
+    LongFractional(u64),
+    IncorectCurrency(String),
+}
+
+  
+
+
+
+
 
 #[cfg(test)]
 mod test {
@@ -110,5 +120,18 @@ mod test {
             let gas = parse_str(test.1, test.2).unwrap();
             assert_eq!(test_data, gas)
         }
+    }
+    #[test]
+    fn parse_errortest() {
+        let test_data = "hnim";
+        let gas = parse_str(test_data, ONE_GIGA_GAS);
+        println!("{:?}", gas);
+        assert_eq!(gas, Err(NearGasParsingError::InvalidNumber("hnim".to_string())) )
+    }
+    #[test]
+    fn parse_u64_errortest(){
+        let test_data = u64::MAX.to_string();
+        let gas = parse_str(&test_data, ONE_GIGA_GAS);
+        assert_eq!(gas, Err(NearGasParsingError::LongWhole(u64::MAX)));
     }
 }
