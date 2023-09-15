6e25f94a80b7e88d63683881e091e443fcca7b8c
iho <iho@users.noreply.github.com>
2023-09-15 09:09:16 +0300

    chore:  Fix test

--- src/lib.rs
+++ src/lib.rs
@@ -329,19 +329,19 @@ mod test {
     #[test]
     #[cfg(feature = "serde")]
     fn borsh() {
-        fn test_borsh_ser(val: u64, raw: &str) {
+        fn test_borsh_ser(val: u64, expected_serialized_value: [u8; 8]) {
             use borsh::to_vec;
             let gas = NearGas::from_gas(val);
             let ser = to_vec(&gas).unwrap();
-            println!("{:?}", ser);
-            assert_eq!(raw, format!("{:?}", ser));
+            // println!("{:?}", ser);
+            assert_eq!(expected_serialized_value, ser.as_slice());
             let de: NearGas = NearGas::try_from_slice(&ser).unwrap();
             assert_eq!(de.as_gas(), val);
         }
 
-        test_borsh_ser(u64::MAX, "[255, 255, 255, 255, 255, 255, 255, 255]");
-        test_borsh_ser(8, "[8, 0, 0, 0, 0, 0, 0, 0]");
-        test_borsh_ser(0, "[0, 0, 0, 0, 0, 0, 0, 0]");
+        test_borsh_ser(u64::MAX, [255, 255, 255, 255, 255, 255, 255, 255]);
+        test_borsh_ser(8, [8, 0, 0, 0, 0, 0, 0, 0]);
+        test_borsh_ser(0, [0, 0, 0, 0, 0, 0, 0, 0]);
     }
 
     #[test]
