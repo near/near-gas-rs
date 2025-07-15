7ba56c87a841c49499b02bd9d9a20b21a1efbeef
polyprogrammist <vad.e.volodin@gmail.com>
2025-07-15 16:09:06 +0800

    tmp

--- src/trait_impls/schemars.rs
+++ src/trait_impls/schemars.rs
@@ -1,5 +1,7 @@
 use crate::NearGas;
+use crate::trait_impls::schemars_exports::schemars;
 
+#[cfg(feature = "schemars-v0_8")]
 impl schemars::JsonSchema for NearGas {
     fn is_referenceable() -> bool {
         false
@@ -10,6 +12,17 @@ impl schemars::JsonSchema for NearGas {
     }
 
     fn json_schema(gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+        String::json_schema(gen)
+    }
+}
+
+#[cfg(feature = "schemars-v1")]
+impl schemars::JsonSchema for NearGas {
+    fn schema_name() -> std::borrow::Cow<'static, str> {
+        String::schema_name()
+    }
+
+    fn json_schema(gen: &mut schemars::SchemaGenerator) -> schemars::Schema {
         String::json_schema(gen)
     }
 }
